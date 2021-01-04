452aca0aa03a3d1d6c108b6eb51d74ec48701475
KingPotty <tpotter98@icloud.com>
2021-01-04 23:23:40 +0000

    Fixed issue preventing scene changes from working. It now uses magic numbers to identify scenes, since using the scene itself as a variable didn't seem to work.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,12 +8,6 @@ public class Score : MonoBehaviour
     public Goals goalLeft;
     public Goals goalRight;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
